c9cae4f64dbc6d437b70671edb2ba4f724ac53ea
Simon (Darkside) Jackson <darkside@zenithmoon.com>
2025-12-12 16:32:36 +0000

    Test

--- src/config.ts
+++ src/config.ts
@@ -3,8 +3,10 @@ import type { MapType } from "./interfaces";
 // Enter your credentials here
 export const CLIENT_ID = "YOUR_CLIENT_ID";
 export const CLIENT_SECRET = "YOUR_CLIENT_SECRET";
-export const CURRENT_MAP_TYPE: MapType = "map";
-export const CODE = "MapCode/MapsetCode";
+// Valid options: "map" | "map-set"
+export const CURRENT_MAP_TYPE: MapType = "YOUR_MAP_TYPE";
+// The map or map-set code to load
+export const CODE = "YOUR_MAP_CODE";
 
 // API endpoints (do not modify)
 export const SDK_AUTH_URL = "https://api.multiset.ai/v1/m2m/token";
